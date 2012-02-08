af2c38cee0561edcee5c080140f0f388390aad1e
Volodymyr Vasylyshyn <vasylyshyn@gmail.com>
2012-02-08 15:32:38 +0200

    Added some structure to the project.

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
--- requires
+-- initialize physics
 require("physics")
-
 physics.start()
+
 -- local variables
-local lime = require("lime")
+local lime = require("lime") -- initialize lime
 local map = lime.loadMap("assets\\maps\\level1a.tmx")
 local physicsData = (require "assets\\physics\\physics").physicsData() -- Add physics objects created in PhysicsEditor
 local layer_hightlight = map:getTileLayer("hightlight")
@@ -12,42 +12,78 @@ local draged_object = nil
 
 display.newRect(0, 0, display.contentWidth, display.contentHeight):setFillColor(165, 210, 255)  -- Create a background colour just to make the map look a little nicer
 
+-- initialize objects from map on it creating
 local onObject = function(object)
 	local layer = map:getTileLayer("windows")
 	if object.type == "Player" then
-		player = display.newImage(layer.group, "assets\\images\\cat.png")
-		player.x, player.y  = object.x - 16, object.y
-		player.start_x, player.start_y  = object.x - 16, object.y
-		physics.addBody(player, { density = 1.0, friction = 1000.0, bounce = 0.0, radius = 7 } )
-		player.bodyType = "static"
-		function player:touch(event)
-			player.bodyType = "dynamic"			
-			player:applyLinearImpulse(0, 1, player.x, player.y)
-		end		
-		player:addEventListener("touch", player)		
+		init_player(object, layer)
 	end
 	if object.type == "mouse" then
-		local mouse = display.newImage(layer.group, "assets\\images\\mouse.png")
-		mouse.x, mouse.y  = object.x, object.y
-		physics.addBody(mouse, { radius = 9 } )
-		--player.bodyType = "static"
-
-		mouse.HasBody = true
-		mouse.IsPickup = true
-		mouse.bodyType = "static"
-		mouse.isSensor = true
-		mouse.pickupType = "mouse"
-		mouse.scoreValue = 50	
+		init_mouse(object, layer)
 	end	
 	if object.type == "tube" then
 		init_tube(object, layer)
 	end
 end
-
 map:addObjectListener("Player", onObject)
 map:addObjectListener("tube", onObject)
 map:addObjectListener("mouse", onObject)
 
+function init_player(object, layer)
+	player = display.newImage(layer.group, "assets\\images\\cat.png")
+	player.x, player.y  = object.x - 16, object.y
+	player.start_x, player.start_y  = object.x - 16, object.y
+	physics.addBody(player, { density = 1.0, friction = 1000.0, bounce = 0.0, radius = 7 } )
+	player.bodyType = "static"
+	function player:touch(event)
+		player.bodyType = "dynamic"			
+		player:applyLinearImpulse(0, 1, player.x, player.y)
+	end	
+	function player:collision(event)
+		if ( event.phase == "began" ) then
+			if event.other.IsPickup then
+				local item = event.other
+				
+				local onTransitionEnd = function(transitionEvent)
+					if transitionEvent["removeSelf"] then
+						transitionEvent:removeSelf()
+					end
+				end
+						
+				-- Fade out the item
+				transition.to(item, {time = 500, alpha = 0, onComplete=onTransitionEnd})
+				
+				local text = nil
+				
+				if item.pickupType == "mouse" then				
+					--text = display.newText( item.scoreValue .. " Points!", 0, 0, "Helvetica", 50 )		
+					text = display.newText("Mouse is dead!", 0, 0, "Helvetica", 50 )		
+				end
+				
+				if text then
+					text:setTextColor(0, 0, 0, 255)
+					text.x = display.contentCenterX
+					text.y = text.height / 2
+					
+					transition.to(text, {time = 2000, alpha = 0, onComplete=onTransitionEnd})
+				end
+			end
+		end
+	end		
+	player:addEventListener("touch", player)
+	player:addEventListener( "collision", player )	
+end
+function init_mouse(object, layer)
+	local mouse = display.newImage(layer.group, "assets\\images\\mouse.png")
+	mouse.x, mouse.y  = object.x, object.y
+	physics.addBody(mouse, { radius = 9 } )
+	mouse.HasBody = true
+	mouse.IsPickup = true
+	mouse.bodyType = "static"
+	mouse.isSensor = true
+	mouse.pickupType = "mouse"
+	mouse.scoreValue = 50		
+end
 function init_tube(object, layer)
 	local tube_type = "tube_" .. object.tube_type
 	local imagePath = "assets\\images\\" .. tube_type .. "_p.png" 
@@ -142,39 +178,3 @@ local globalTouchListener = function( event )
 	end
 end 
 Runtime:addEventListener( "touch", globalTouchListener )
-
--- here will be 
-local function onCollision(self, event )
- 	if ( event.phase == "began" ) then
-		if event.other.IsPickup then
-			local item = event.other
-			
-			local onTransitionEnd = function(transitionEvent)
-				if transitionEvent["removeSelf"] then
-					transitionEvent:removeSelf()
-				end
-			end
-					
-			-- Fade out the item
-			transition.to(item, {time = 500, alpha = 0, onComplete=onTransitionEnd})
-			
-			local text = nil
-			
-			if item.pickupType == "mouse" then				
-				--text = display.newText( item.scoreValue .. " Points!", 0, 0, "Helvetica", 50 )		
-				text = display.newText("Mouse is dead!", 0, 0, "Helvetica", 50 )		
-			end
-			
-			if text then
-				text:setTextColor(0, 0, 0, 255)
-				text.x = display.contentCenterX
-				text.y = text.height / 2
-				
-				transition.to(text, {time = 2000, alpha = 0, onComplete=onTransitionEnd})
-			end
-		end
-	end
-end
-
-player.collision = onCollision
-player:addEventListener( "collision", player )
